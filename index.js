498bd16b203d6b7effdcbf6b9012c38fe15e3aab
Andres Alvarez <andres.alvarez094@hotmail.es>
2022-08-23 21:38:40 -0500

    create middleware to handle errors

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const { logErrors, errorHandler } = require('./middlewares/error.handler');
 const routesAPI = require('./routes');
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(logErrors);
+app.use(errorHandler);
 routesAPI(app);
 
 app.listen(port, () => console.log(`[Run Server]  http://localhost:${port}`));
