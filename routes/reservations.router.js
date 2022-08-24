4944c627186e001782fa1d65e35843005e8c92e2
Andres Alvarez <andres.alvarez094@hotmail.es>
2022-08-24 09:38:34 -0500

    the same to the last commit

--- routes/reservations.router.js
+++ routes/reservations.router.js
@@ -2,9 +2,13 @@ const express = require('express');
 const reservationService = require('../services/reservations.service');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    const data = reservationService.find();
-    res.status(200).json(data);
+router.get('/', (req, res, next) => {
+    try {
+        const data = reservationService.find();
+        res.json(data);
+    } catch (error) {
+        next(error);
+    }
 });
 
 module.exports = router;
