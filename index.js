0f19e8067c31d5b9e64a720e122eb678f50bac86
Andres Alvarez <andres.alvarez094@hotmail.es>
2022-08-25 16:02:39 -0500

    add Procfile

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const routesAPI = require('./routes');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
